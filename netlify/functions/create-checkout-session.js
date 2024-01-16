970c3af9c03e8a8d7a4a90ed9155aab4692d766d
Bobby Davidson <bobbyjames839@gmail.com>
2024-01-16 18:34:29 +0000

    Initial commit

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,18 +1,19 @@
-// netlify/functions/create-checkout-session.js
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [{
-      price_data: {
+      'price': 'price_1OOQElGbVAl2XET16cyOo0pS',
+      'quantity': 1,
+      /*price_data: {
         currency: 'usd',
         product_data: {
           name: 'Your Product Name',
         },
-        unit_amount: 2799, // your product price
+        unit_amount: 2799, 
       },
-      quantity: 1,
+      quantity: 1,*/
     }],
     mode: 'payment',
     success_url: `${process.env.YOUR_DOMAIN}/success`,
