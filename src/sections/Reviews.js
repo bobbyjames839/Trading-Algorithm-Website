6668960bdf8ddbf0a84c891e5f0147b8221007a3
Bobby Davidson <bobbyjames839@gmail.com>
2024-07-15 22:39:58 +0100

    update

--- src/sections/Reviews.js
+++ src/sections/Reviews.js
@@ -24,7 +24,7 @@ export const Reviews = () => {
         <ReviewItem name='John Paul' title='Simple to use!' desc='This trading indicator is very effective and user-friendly. As a beginner I have foudn it useful and I highly recommend it.'/>
         <ReviewItem name='Jane Smith' title='Highly effective!' desc='This trading indicator has significantly improved my trading performance. It is easy to understand and apply.'/>
         <ReviewItem name='Robert Johnson' title='Excellent tool!' desc='I have found this trading indicator to be extremely useful. It provides accurate signals and has helped me make profitable trades.'/>
-        <ReviewItem name='Emily Turner' title='Remarkable!' desc="The accuracy of this trading indicator is outstanding. It's been a game-changer for my investment strategy, and I've seen great returns. Definitely a must-have for serious traders."/>
+        <ReviewItem name='Emily Turner' title='Remarkable!' desc="this is a fake review"/>
       </div>
     </div>
   )
